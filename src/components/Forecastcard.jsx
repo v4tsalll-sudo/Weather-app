bb7e740fe5d68ead43d3e99e27ac43cebf4767a7
v4tsalll <v4tsalll@gmail.com>
2026-02-02 15:50:39 +0530

    third commit

--- src/components/Forecastcard.jsx
+++ src/components/Forecastcard.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Forecastcard = ({ date, icn,min, max }) => {
         return (
-          <div className="rounded-2xl  p-4 text-center min-w-20 border border-white/20 bg-white/10 shadow-lg backdrop-blur-[5px] ">
+          <div className="rounded-2xl  p-4 text-center min-w-25 border border-white/20 bg-white/10 shadow-lg backdrop-blur-[5px] ">
             <p className="text-sm">
               {new Date(date).toLocaleDateString('en-US',{weekday: 'short'})}
             </p>
             <img src={`https://openweathermap.org/img/wn/${icn}@2x.png`} alt="" />
-            <p className="text-xs opacity-80  text-center">{min}°/{max}°</p>
+            <p className="text-xs opacity-80 m ">{min}°/{max}°</p>
           </div>
   )
 }
